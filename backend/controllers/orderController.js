56de5c773619d679b484dc7a69817d4e5ca5e4a9
ajisrael <ajisrael@mtu.edu>
2021-03-16 15:51:57 -0400

    d   uppercase Private in access header

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,7 +3,7 @@ import Order from '../models/orderModel.js';
 
 // @desc    Create new order
 // @route   POST /api/orders
-// @access  private
+// @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
