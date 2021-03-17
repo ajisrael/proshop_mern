439e2278d41dd2cf9f5e4ebf51429e4beab1d7da
ajisrael <ajisrael@mtu.edu>
2021-03-17 13:59:28 -0400

    B   removed isAdmin dependency in req body

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -150,7 +150,12 @@ const updateUser = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    user.isAdmin = req.body.isAdmin;
+
+    if (req.body.isAdmin) {
+      user.isAdmin = true;
+    } else {
+      user.isAdmin = false;
+    }
 
     const updatedUser = await user.save();
 
